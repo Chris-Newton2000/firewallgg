7546f6a6cd8b28f29e11ea81a1f0bbe5f079f6d6
GregoryDev <95302476+Chris-Newton2000@users.noreply.github.com>
2022-07-12 23:38:10 +0200

    Update requestPHP.php
    From GregoryDev#6527

--- examples/requestPHP.php
+++ examples/requestPHP.php
@@ -1,1 +1,12 @@
+<?php 
+$httprequest = file_get_contents("https://firewall.hyperz.net/api/checkuser/<?php echo $input ?>");
+$input = $_POST['userid'];
+?>
+<html>
+<body>
+User ID<input type="text" name="userid"><br>
+<input type="submit">
+</form>
+</body>
+</html>
 
